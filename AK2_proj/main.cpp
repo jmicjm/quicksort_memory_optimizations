ed7076ba52ad9f32ff1ada8f23468b541257ab4f
Jakub Michalski <jakub.michalski2000@outlook.com>
2022-09-14 17:23:17 +0200

    Add various datasets

--- AK2_proj/main.cpp
+++ AK2_proj/main.cpp
@@ -26,10 +26,72 @@ int rval(int min, int max)
 	return std::uniform_int_distribution<>{min, max}(rng);
 }
 
+template<typename T>
+void testAll(const data_samples<T>& data_samples);
+
+
+int main()
+{
+	constexpr auto sample_count = 1;
+	constexpr auto sample_size = 1e6;
+
+	data_samples<std::string> random_data_samples(sample_count);
+	for (auto& sample : random_data_samples)
+	{
+		for (int i = 0; i < sample_size; i++)
+		{
+			std::string str(rval(1, 16), ' ');
+			for (auto& ch : str) ch = rval('a', 'z');
+
+			sample.push_back(str);
+		}
+	}
+	std::cout << "==random data order\n";
+	testAll(random_data_samples);
+
+	auto increasing_data_samples = std::move(random_data_samples);	
+	for (auto& sample : increasing_data_samples)
+	{
+		std::sort(sample.begin(), sample.end());
+	}
+	std::cout << "==increasing data order\n";
+	testAll(increasing_data_samples);
+
+	auto decreasing_data_samples = std::move(increasing_data_samples);
+	for (auto& sample : decreasing_data_samples)
+	{
+		std::sort(sample.begin(), sample.end(), [](const auto& a, const auto& b) { return a >= b; });
+	}
+	std::cout << "==decreasing data order\n";
+	testAll(decreasing_data_samples);
+
+	data_samples<std::string> equal_data_samples(sample_count);
+	for (auto& sample : equal_data_samples)
+	{
+		for (int i = 0; i < sample_size; i++)
+		{
+			sample.push_back(std::string(16, 'a'));
+		}
+	}
+	std::cout << "==equal data\n";
+	testAll(equal_data_samples);
+
+	data_samples<std::string> interleaved_data_samples(sample_count);
+	for (auto& sample : interleaved_data_samples)
+	{
+		for (int i = 0; i < sample_size; i++)
+		{
+			sample.push_back(std::string(16, i%2 ? 'a': 'b'));
+		}
+	}
+	std::cout << "==interleaved data\n";
+	testAll(interleaved_data_samples);
+}
+
 template<typename T, typename qsort_f>
 void test(data_samples<T> samples, qsort_f qsort, std::string_view description)
 {
-	auto sum_sp = 0;
+	auto sum_stack = 0;
 	auto sum_heap = 0;
 
 	auto sorted = true;
@@ -48,32 +110,18 @@ void test(data_samples<T> samples, qsort_f qsort, std::string_view description)
 		time += std::chrono::duration_cast<std::chrono::milliseconds>(e - b).count();
 
 		sorted &= std::is_sorted(sample.begin(), sample.end());
-
-		sum_sp += (sp - max_sp);
+		sum_stack += (sp - max_sp);
 		sum_heap += max_heap;
 	}
 
-	std::cout << description << ":\nstack - " << sum_sp / samples.size() << " heap - " << sum_heap / samples.size() << "\n";
+	std::cout << description << ":\nstack - " << sum_stack / samples.size() << " heap - " << sum_heap / samples.size() << "\n";
 	std::cout << time / samples.size() << " ms\n";
 	std::cout << "sorted?: " << sorted << "\n\n";
 }
 
-
-int main()
+template<typename T>
+void testAll(const data_samples<T>& data_samples)
 {
-	data_samples<std::string> data_samples(16);
-
-	for (auto& sample : data_samples)
-	{
-		for (int i = 0; i < 1e6; i++)
-		{
-			std::string str(rval(1, 16), ' ');
-			for (auto& ch : str) ch = rval('a', 'z');
-
-			sample.push_back(str);
-		}
-	}
-
 	test(data_samples, qsortRecursive<std::string, partition>, "recursive");
 	test(data_samples, qsortRecursive<std::string, inlinePartition>, "recursive,\ninline partition");
 
@@ -100,6 +148,5 @@ int main()
 
 	test(data_samples, qsortIterativeNoRedundancyStack<std::string, partitionNoPivotCopy>, "iterative no redundancy stack,\nno pivot copy");
 	test(data_samples, qsortIterativeNoRedundancyStack<std::string, inlinePartitionNoPivotCopy>, "iterative no redundancy stack,\ninline partition,\nno pivot copy");
-
-	return 0;
 }
+
